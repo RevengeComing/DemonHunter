cdfe6f0198fffcfee472e2ab4d065122b84eb27a
Sepehr Hamzelooy <s.hamzelooy@gmail.com>
2017-05-09 00:43:29 +0430

    document updated

--- examples/manager/simple_manager.py
+++ examples/manager/simple_manager.py
@@ -1,16 +1,16 @@
 import asyncio
-
 from demonhunter import Manager
 
 loop = asyncio.get_event_loop()
 
 # You can set logfile location to any place like "/var/log/demonhunter.log"
-manager = Manager(loop, logfile='test.log')
+# specify management IP via server_address
+# manager = Manager(loop, server_address='10.10.10.10', logfile='events.log')
+manager = Manager(loop, server_address=None, logfile='events.log')
 # Specify The Location of Agents ?
-manager.add_agent_address('127.0.0.1')
+manager.add_agent_address("107.170.73.18")
 
 try:
-    # Run The Loop
     loop.run_forever()
 except KeyboardInterrupt:
     print("\nServer Closed")
